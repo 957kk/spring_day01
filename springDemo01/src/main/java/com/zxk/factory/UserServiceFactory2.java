7d06c2db1abd68caf6144d3189ff2b1ba19d3cb7
9527mmm <45967247+9527mmm@users.noreply.github.com>
2021-09-01 20:48:36 +0800

    dev01

--- springDemo01/src/main/java/com/zxk/factory/UserServiceFactory2.java
+++ springDemo01/src/main/java/com/zxk/factory/UserServiceFactory2.java
@@ -13,6 +13,7 @@ import com.zxk.service.impl.UserServiceImpl;
 public class UserServiceFactory2 {
     public  UserService getService(){
         System.out.println("实例工厂创建");
+        System.out.println("new ");
         return new UserServiceImpl();
     }
 }
